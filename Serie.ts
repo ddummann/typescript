f80bd0f9d533d77ed21d30e023ffa66289eb5a53
ddummann <duartejm310@gmail.com>
2022-04-15 00:05:06 -0500

    tutorial finished

---
+++ Serie.ts
@@ -0,0 +1,21 @@
+class Serie {
+
+    pos: number;
+    name: string;
+    channel: string;
+    seasons: number;
+    description: string;
+    link: string;
+    image: string;
+
+    constructor(pos: number, name: string, channel: string, seasons: number, 
+        description: string, link: string, image: string) {
+        this.pos = pos;
+        this.name = name;
+        this.channel = channel;
+        this.seasons = seasons;
+        this.description = description;
+        this.link = link;
+        this.image = image;
+    }
+}
