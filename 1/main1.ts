e43fa8ab526bc4c88abf8a31495e004a5abd131b
ddummann <duartejm310@gmail.com>
2022-04-15 02:58:33 -0500

    me faltaba la segunda parte jiji :) done.

--- 1/main1.ts
+++ 1/main1.ts
@@ -1,12 +1,14 @@
 console.log('Iniciando aplicación');
 
+
 import{ dataSeries } from './dataSeries.js';   // import the data from dataSeries.ts
 import { Serie } from './Serie.js';  //import the class Serie from Serie.js
 
 let seriesTbody: HTMLElement = document.getElementById('series')!;
 const avgSeasonsElm: HTMLElement = document.getElementById("avg-seasons")!;
 
-console.log('Desplegando funcion series');
+avgSeasonsElm.innerHTML = `${getAvgSeasons(dataSeries)}`
+console.log(avgSeasonsElm);
 renderSeriesInTable(dataSeries);
 
 function renderSeriesInTable(series: Serie[]): void {
@@ -20,3 +22,23 @@ function renderSeriesInTable(series: Serie[]): void {
         seriesTbody.appendChild(trElement);
     });
 }
+
+/*
+    funcion para calcular el promedio de los temporadas
+    @param series: array de series
+    @returns promedio de los temporadas
+
+*/
+
+function getAvgSeasons(series: Serie[]): number {
+    let sum: number = 0;
+    series.forEach((serie) => {
+        sum += serie.seasons;
+    });
+    return sum / series.length;
+}
+
+
+
+
+
--- 1/scripts/main1.js
+++ 1/scripts/main1.js
@@ -2,7 +2,8 @@ console.log('Iniciando aplicación');
 import { dataSeries } from './dataSeries.js'; // import the data from dataSeries.ts
 var seriesTbody = document.getElementById('series');
 var avgSeasonsElm = document.getElementById("avg-seasons");
-console.log('Desplegando funcion series');
+avgSeasonsElm.innerHTML = "".concat(getAvgSeasons(dataSeries));
+console.log(avgSeasonsElm);
 renderSeriesInTable(dataSeries);
 function renderSeriesInTable(series) {
     console.log('Desplegando series');
@@ -11,4 +12,17 @@ function renderSeriesInTable(series) {
         trElement.innerHTML = "<td>".concat(serie.pos, "</td>\n                               <td>").concat(serie.name, "</td>\n                               <td>").concat(serie.channel, "</td>\n                               <td>").concat(serie.seasons, "</td>");
         seriesTbody.appendChild(trElement);
     });
+}
+/*
+    funcion para calcular el promedio de los temporadas
+    @param series: array de series
+    @returns promedio de los temporadas
+
+*/
+function getAvgSeasons(series) {
+    var sum = 0;
+    series.forEach(function (serie) {
+        sum += serie.seasons;
+    });
+    return sum / series.length;
 }
